b3abb04ceb9e3c40d718b154a24bc97338d6cff2
Edrey De Aza <edreydeaza1515@gmail.com>
2023-01-08 02:53:03 -0400

    v1.1.16 - instalando paquetes

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'postgres'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'password'
 const DB_NAME = process.env.DB_NAME || 'redsocial'
-const DB_PORT = process.env.DB_PORT || '5432'
+const DB_PORT = process.env.DB_PORT || 5432
 
 module.exports = {
     PORT,
